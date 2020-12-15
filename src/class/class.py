e83fdb13e2ae376d0a9248873800665a36beb654
edwardpassagi <edward.passagi@gmail.com>
2020-12-14 20:48:18 -0600

    added class object for variable storage

---
+++ src/class/class.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.insert(1, './')
+
+class unigramValues:
+    P_pos = 0.0
+    P_neg = 0.0
+    dict_pos = {}
+    dict_neg = {}
+    P_UNK_POS = 0.0
+    P_UNK_NEG = 0.0
+    
+class bigramValues:
+    P_BI_POS = 0.0
+    P_BI_NEG = 0.0
+    dict_BI_pos = {}
+    dict_BI_neg = {}
+    P_UNK_BI_POS = 0.0
+    P_UNK_BI_NEG = 0.0
+    
